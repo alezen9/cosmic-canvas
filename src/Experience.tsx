f49ef168c538e3eb0585b048e4304b03ef8cadea
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2024-11-15 14:30:44 +0100

    feat: replace environment map with drei starfield

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,17 +1,29 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import Jupiter from "./components/Jupiter/Jupiter";
-import { Suspense } from "react";
+import { useEffect } from "react";
+import { useThree } from "@react-three/fiber";
 
 const Experience = () => {
+  const camera = useThree(({ camera }) => camera);
+  useEffect(() => {
+    console.log(camera);
+  }, [camera]);
+
   return (
     <>
       <Perf position="top-left" />
       <OrbitControls makeDefault />
       <color attach="background" args={["black"]} />
-      <Suspense>
-        <Environment background files="./outer-space.jpg" />
-      </Suspense>
+      <Stars
+        radius={20}
+        depth={20}
+        count={500}
+        factor={3}
+        saturation={0}
+        fade
+        speed={1}
+      />
       <Jupiter />
     </>
   );
