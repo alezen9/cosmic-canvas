95c25678eea99a69b8edc607fc42285d242d641d
Aleksandar Gjoreski <aleksandar.d.gjoreski@gmail.com>
2024-11-16 17:51:27 +0100

    chore: hide leva if url does not have hash debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 import { Leva } from "leva";
+import { useState } from "react";
 
 const App = () => {
+  const [isDebug] = useState(() => window.location.hash === "#debug");
+
   return (
     <>
       <div className="leva-wrapper">
-        <Leva collapsed />
+        <Leva collapsed hidden={!isDebug} />
       </div>
       <Canvas
         dpr={[1, 2]}
